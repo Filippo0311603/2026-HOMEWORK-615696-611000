8d00319a87fa39200ceb18e21cbd335d3c178993
Filippo0311603 <118382768+Filippo0311603@users.noreply.github.com>
2026-03-23 13:50:55 +0100

    Test Stanza

---
+++ Test/StanzaTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StanzaTest {
+	
+	
+	Stanza s1=new Stanza("Bagno");
+	Attrezzo a1= new Attrezzo("Martello",8);
+	Attrezzo a2= new Attrezzo("Spada",8);
+	Attrezzo a3= new Attrezzo("Coltello",8);
+	Attrezzo a4= new Attrezzo("Ascia",8);
+	
+	boolean b=s1.addAttrezzo(a1);
+	boolean b2=s1.addAttrezzo(a2);
+	boolean b3=s1.addAttrezzo(a3);
+	boolean b4=s1.addAttrezzo(a4);
+	@Test
+	public void test() {
+		assertTrue(b);
+	}
+	
+	@Test
+	public void test2() {
+		assertEquals(4,s1.getNumeroAttrezzi());
+	}
+	
+	@Test
+	public void test3() {
+		assertEquals(4,s1.getNumeroAttrezzi());
+		boolean b2=s1.removeAttrezzo(a1);
+		assertEquals(3,s1.getNumeroAttrezzi());
+	}
+
+}
--- src/Stanza.java
+++ src/Stanza.java
@@ -170,8 +170,19 @@ public class Stanza {
 	 * @return true se l'attrezzo e' stato rimosso, false altrimenti
 	 */
 	public boolean removeAttrezzo(Attrezzo attrezzo) {
-		// TODO da implementare
-		return false;
+		boolean b=false;
+		for(int i=0;i<attrezzi.length;i++) {
+			if(attrezzi[i]!=null && attrezzi[i].getNome().equals(attrezzo.getNome())) {
+				attrezzi[i]=null;
+				numeroAttrezzi--;
+				b=true;
+			}
+		}
+		return b;
+	}
+	
+	public int getNumeroAttrezzi() {
+		return this.numeroAttrezzi;
 	}
 
 
